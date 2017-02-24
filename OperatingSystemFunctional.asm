3b8f2001b07c94ce7008feb3e41d57e986d31f71
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-02-24 16:32:25 -0500

    virus editiion

---
+++ OperatingSystemFunctional.asm
@@ -0,0 +1,87 @@
+	org 0x7c00
+	jmp short start
+	nop
+bsOEM	db "OS423 v.0.1"               ; OEM String
+
+start:
+	call cls;
+	call printSplash;
+	call second;
+	int 20h;	
+
+second:
+  push eax;
+  push ebx;
+
+  xor bx, bx;
+
+  mov bx, 0x0012;
+  mov es, bx;
+  mov bx, 0x1234;
+
+  mov ah, 0x02;
+  mov al, 0x01;
+  mov ch, 0x01;
+  mov cl, 0x02;
+  mov dh, 0x00;
+  mov dl, 0x00;
+  int 0x13;
+  jc second;
+  jmp 0x0012:0x1234;
+  ret;
+
+cls:
+	push ebx;	
+	push eax;	
+
+	mov ah,06h		;Function 06h (scroll screen)
+	mov al,0		;Scroll all lines
+	mov bh,0ah		;Attribute (lightgreen on black) 
+	mov ch,0		;Upper left row is zero
+	mov cl,0		;Upper left column is zero
+	mov dh,24		;Lower left row is 24
+	mov dl,79		;Lower left column is 79
+	int 10h			;BIOS Interrupt 10h (video services)
+				;Colors from 0: Black Blue Green Cyan Red Magenta Brown White
+				;Colors from 8: Gray LBlue LGreen LCyan LRed LMagenta Yellow BWhite
+	
+	pop ebx;	
+	pop eax;		; return val to ah, al
+	ret;   			; return to calling mode
+
+printSplash:
+	mov ah,13h		;Function 13h (display string), XT machine only
+	mov al,1		;Write mode is zero: cursor stay after last char
+	mov bh,0		;Use video page of zero
+	mov bl,0ah		;Attribute (lightgreen on black)
+	mov cx,mlen		;Character string length
+	mov dh,9		;Position on row 9 
+	mov dl,29		;And column 29 
+	lea bp,[msg] 		;Load the offset address of string into BP, es:bp
+				;Same as mov bp, msg  
+	int 10h
+
+	ret;
+	
+	;32 is space
+	;205 is double horizontal
+	;186 is double vertical 
+	;201 is top left corner
+	;200 is bot left corner 
+	;188 is bot right corner
+	;187 is top right corner
+	;30 across
+	
+
+
+msg db 201,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,187,10,13,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,186,32,'Zac',39,'s OS version 0.1',32,186,10,13,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,200,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,188,10,13,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,'$';
+
+mlen equ $-msg
+
+dollar db '$'
+dollarLen equ $-dollar
+
+
+padding	times 510-($-$$) db 0		;to make MBR 512 bytes
+bootSig	db 0x55, 0xaa		;signature (optional)
+
--- zacOS.asm
+++ zacOS.asm
@@ -1,87 +1,69 @@
+; OS423 Master Boot Record Sector
+; ver03 -- by video memory b8000 -- with para
+; 1/5/2016
+
+	;bit16					; 16bit by default
 	org 0x7c00
 	jmp short start
 	nop
 bsOEM	db "OS423 v.0.1"               ; OEM String
 
 start:
-	call cls;
-	call printSplash;
-	call second;
-	int 20h;	
-
-second:
-  push eax;
-  push ebx;
-
-  xor bx, bx;
-
-  mov bx, 0x0012;
-  mov es, bx;
-  mov bx, 0x1234;
-
-  mov ah, 0x02;
-  mov al, 0x01;
-  mov ch, 0x01;
-  mov cl, 0x02;
-  mov dh, 0x00;
-  mov dl, 0x00;
-  int 0x13;
-  jc second;
-  jmp 0x0012:0x1234;
-  ret;
-
-cls:
-	push ebx;	
-	push eax;	
-
-	mov ah,06h		;Function 06h (scroll screen)
-	mov al,0		;Scroll all lines
-	mov bh,0ah		;Attribute (lightgreen on black) 
-	mov ch,0		;Upper left row is zero
-	mov cl,0		;Upper left column is zero
-	mov dh,24		;Lower left row is 24
-	mov dl,79		;Lower left column is 79
-	int 10h			;BIOS Interrupt 10h (video services)
-				;Colors from 0: Black Blue Green Cyan Red Magenta Brown White
-				;Colors from 8: Gray LBlue LGreen LCyan LRed LMagenta Yellow BWhite
+		
+;;cls
+	mov bh, 0ah		; bh has color attribute
+	mov cl, bh		; needs es:bx for display, save bh value	
 	
-	pop ebx;	
-	pop eax;		; return val to ah, al
-	ret;   			; return to calling mode
+	mov bx,0xb800		;direct video memory access 0xB8000
+	mov es,bx
+	xor bx,bx		;es:bx : 0xb8000
+	mov dh,0		;row from 0 to 24
+	mov dl,0		;col from 0 to 79
+		
+.loop:
+	mov byte [es:bx], ' '	;char
+	inc bx
+	mov byte [es:bx], cl
+	inc bx
 
-printSplash:
-	mov ah,13h		;Function 13h (display string), XT machine only
-	mov al,1		;Write mode is zero: cursor stay after last char
-	mov bh,0		;Use video page of zero
-	mov bl,0ah		;Attribute (lightgreen on black)
-	mov cx,mlen		;Character string length
-	mov dh,9		;Position on row 9 
-	mov dl,29		;And column 29 
-	lea bp,[msg] 		;Load the offset address of string into BP, es:bp
-				;Same as mov bp, msg  
-	int 10h
+.next:		
+	inc dl
+	cmp dl,80		;col 0-79
+	jne .loop
+	mov dl,0
+	inc dh
+	cmp dh,25		;row 0-24
+	jne .loop		
+		
+; display 'Hello 423' at (0,0)
 
-	ret;
+print:
+	mov bl, 0ah		;color
+	mov cx, mlen
+	lea bp, [msg]
+
+	mov al, bl		;save color
+	mov bx,0xb800	;direct video memory access 0xB8000
+	mov es,bx
+	xor bx,bx
+				
+.loop:
+	mov ah, byte [ds:bp]
+	mov byte [es:bx], ah	;char
+	inc bx
+	mov byte [es:bx], al	;attribute 
+	inc bx
+
+.next:	
+	inc bp
+	dec cx
+	jne .loop		
+
 	
-	;32 is space
-	;205 is double horizontal
-	;186 is double vertical 
-	;201 is top left corner
-	;200 is bot left corner 
-	;188 is bot right corner
-	;187 is top right corner
-	;30 across
+	int 20h
+
+msg db 'Hello 423'
+mlen equ $-msg	
 	
-
-
-msg db 201,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,187,10,13,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,186,32,'Zac',39,'s OS version 0.1',32,186,10,13,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,200,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,188,10,13,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,'$';
-
-mlen equ $-msg
-
-dollar db '$'
-dollarLen equ $-dollar
-
-
 padding	times 510-($-$$) db 0		;to make MBR 512 bytes
 bootSig	db 0x55, 0xaa		;signature (optional)
-
