e7242c6b06025a7d1f42154a9ff783ed516c752b
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-02-28 17:08:20 -0500

    updates with pause

--- zacOS.asm
+++ zacOS.asm
@@ -1,41 +1,64 @@
 ; OS423 Master Boot Record Sector
 ; ver03 -- by video memory b8000 -- with para
-; 1/5/2016
+; dh is height dl is length of row of printing
 
-	;bit16					; 16bit by default
+	;bit16			;16bit by default
 	org 0x7c00
 	jmp short start
 	nop
-bsOEM	db "OS423 v.0.1"       	; OEM String
+bsOEM	db "OS423 v.0.1"       	;OEM String
 
 start:
 		
-;;cls
-	mov cl, 0ah		; bh has color attribute
-	mov cl, bh		; needs es:bx for display, save bh value	
+;;cls:
+	mov cl, 0ah			;bh has color attribute
+	mov cl, bh			;needs es:bx for display, save bh value	
 	
 	mov bx,0xb800		;direct video memory access 0xB8000
 	mov es,bx
-	xor bx,bx		;es:bx : 0xb8000
-	mov dh,0		;row from 0 to 24
-	mov dl,0		;col from 0 to 79
+	xor bx,bx			;es:bx : 0xb8000
+	mov dh,0			;row from 0 to 24
+	mov dl,0			;col from 0 to 79
 		
 .loop:
+						;fill with all null characters
 	mov byte [es:bx], ''	;char
 	inc bx
 	mov byte [es:bx], cl
 	inc bx
 
 .next:		
-	inc dl 			;dl is the length/row movement.
-	cmp dl,80		;col 0-79
-	jne .loop		;go back to .loop
-	mov dl,0		;
-	inc dh			;go down vertically a line 
-	cmp dh,25		;row 0-24
-	jne .loop		
-		
-; display 'Hello 423' at (0,0)
+	inc dl 				;dl is the length/row movement.
+	cmp dl,80			;col 0-79
+	jne .loop			;go back to .loop
+	mov dl,0		
+	inc dh				;go down vertically a line 
+	cmp dh,25			;row 0-24
+	jne .loop	
+	call pause	
+	jmp print		; jmp print here
+
+;display 'Hello 423' at (0,0)
+ pause:
+	push edx;
+	push ecx;
+	xor cx, cx;
+	xor dx, dx;
+.out:
+	xor dx, dx;
+	inc cx;
+	cmp cx, 256;
+	je .exit;
+.inner:
+	inc dx;
+	nop;
+	cmp dx, 256;
+	je .out;
+
+.exit:
+	pop ecx;
+	pop edx;
+	ret;
 
 print:
 	mov bl, 0ah		;color
@@ -43,7 +66,7 @@ print:
 	lea bp, [msg]
 
 	mov al, bl		;save color
-	mov bx,0xb800		;direct video memory access 0xB8000
+	mov bx,0xb800	;direct video memory access 0xB8000
 	mov es,bx
 	xor bx,bx
 				
@@ -66,4 +89,4 @@ msg db 'Hello 423'
 mlen equ $-msg	
 	
 padding	times 510-($-$$) db 0		;to make MBR 512 bytes
-bootSig	db 0x55, 0xaa		;signature (optional)
+bootSig	db 0x55, 0xaa		 	;signature (optional)
